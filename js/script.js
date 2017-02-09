3d938d0d9788f01aab13cb0eecdc5123be5192fe
Felix Martel <Felix Martel>
2017-02-09 11:00:50 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -60,6 +60,23 @@ function fadeout(){
 	}}, interval);
 }
 
+function readSongInfo(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+/*
+readSongInfo("../data.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});*/
 
 var triggerSleep = document.getElementById('sleep');
 triggerSleep.addEventListener("click", fadeout);
