90ee6d21443770c1765668ff82480239df9647b9
Felix Martel <Felix Martel>
2017-03-02 13:50:34 +0100

    Ajout du player waveform

--- js/player.js
+++ js/player.js
@@ -1,10 +1,13 @@
-
+//var serverURL = 'http://localhost:7080/xray/';
+var audioURL = 'audio/demo.mp3';
 var ws = WaveSurfer.create({
   container: '#waveform',
   waveColor: 'rgba(255, 255, 255, 0.7)',
   progressColor: 'white',
+  cursorColor: 'transparent',
 });
-ws.load('/audio/demo.mp3');
+ws.load(audioURL);
 ws.on('ready', function (){
-  wavesurfer.play();
-})
+  ws.setVolume(0.03);
+  //ws.play();
+});
